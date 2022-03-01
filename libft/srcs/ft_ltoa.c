a35457c7902af0fc726ffb41dcd0bdb245c4387d
YamasouA <akiakiskyhand@gmail.com>
2022-03-02 00:41:47 +0900

    add ltoa function

--- libft/srcs/ft_ltoa.c
+++ libft/srcs/ft_ltoa.c
@@ -14,57 +14,44 @@
 
 static	size_t	count(long long n)
 {
-	long	n_cpy;
 	size_t		cnt;
 
-	n_cpy = (long)n;
 	cnt = 0;
-	if (n_cpy == 0)
+	if (n == 0)
 		return (1);
-	if (n_cpy < 0)
-	{
-		n_cpy *= -1;
+	if (n < 0)
 		cnt++;
-	}
-	while (n_cpy > 0)
+	while (n != 0)
 	{
 		cnt++;
-		n_cpy /= 10;
+		n /= 10;
 	}
 	return (cnt);
 }
 
-static	void	add_list(char *ptr, long n, int pos)
-{
-	if (n <= 9)
-		ptr[pos] = n + '0';
-	else
-	{
-		add_list(ptr, n / 10, pos - 1);
-		ptr[pos] = n % 10 + '0';
-	}
-}
-
 char	*ft_ltoa(long long n)
 {
-	long long	n_cpy;
 	size_t		len;
 	char	*ptr;
-	char	*ptr_cpy;
+    int index;
+    int sign;
 
+    sign = n < 0;
 	len = count(n);
-	ptr = (char *)ft_calloc(len + 1, sizeof(char));
+	ptr = (char *)calloc(len + 1, sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	ptr_cpy = ptr;
-	if (n_cpy < 0)
-	{
-		n_cpy *= -1;
+    while (len-->0)
+    {
+        index = - (n % 10);
+        if (n > 0)
+            index = n % 10;
+        ptr[len] = "0123456789"[index];
+        n /= 10;
+    }
+    if (sign)
 		ptr[0] = '-';
-	}
-	add_list(ptr, n_cpy, len - 1);
-	ptr += len;
-	*ptr = '\0';
-	return (ptr_cpy);
+	return (ptr);
 }
+
 
